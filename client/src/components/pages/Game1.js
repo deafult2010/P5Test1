539287a6431abe0616105c3431f31706ccfc016f
deafult2010 <thomas.jaeger@blueyonder.co.uk>
2020-05-14 12:31:29 +0100

    added click

--- client/src/components/pages/Game1.js
+++ client/src/components/pages/Game1.js
@@ -5,12 +5,18 @@ import sketch5 from './sketch5';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const myImage = require('./sprites/Stick.png');
+const myImage2 = require('./sprites/StickHeadShot.png');
 const myJSON = require('./sprites/stick.json');
 
 const Game1 = () => {
   return (
     <div>
-      <P5Wrapper sketch={sketch5} image={myImage} json={myJSON} />
+      <P5Wrapper
+        sketch={sketch5}
+        image={myImage}
+        image2={myImage2}
+        json={myJSON}
+      />
     </div>
   );
 };
--- client/src/components/pages/sketch5.js
+++ client/src/components/pages/sketch5.js
@@ -1,26 +1,40 @@
 export default function sketch5(p) {
   let spritesheet;
+  let spritesheet2;
   let spritedata;
   let imageX;
+  let imageY;
   let animation = [];
+  let animation2 = [];
   let sticks = [];
   let check = true;
   let check2 = true;
+  let click = false;
+  let click2 = false;
 
   p.myCustomRedrawAccordingToNewPropsHandler = function (props) {
     if (props.image) {
       imageX = props.image;
     }
+    if (props.image2) {
+      imageX = props.image;
+      imageY = props.image2;
+    }
     if (props.json) {
       spritedata = props.json;
     }
     spritesheet = p.loadImage(imageX);
+    spritesheet2 = p.loadImage(imageY);
     // No need to use p.loadJSON since I can pass json through props.
     // spritedata = p.loadJSON(jsonX);
   };
 
   p.setup = function () {
     p.createCanvas(400, 400);
+    //disables "context menu" on right click for the canvas
+    p.canvas.oncontextmenu = function (e) {
+      e.preventDefault();
+    };
   };
 
   p.draw = function () {
@@ -32,9 +46,11 @@ export default function sketch5(p) {
           for (let i = 0; i < 30; i++) {
             sticks[i] = new Sprite(
               animation,
+              animation2,
               0,
               i * p.random(10, 30),
-              p.random(0.3, 1)
+              p.random(0.3, 1),
+              true
             );
           }
           check2 = false;
@@ -43,51 +59,118 @@ export default function sketch5(p) {
           for (let stick of sticks) {
             stick.show();
             stick.animate();
+            stick.clicked();
+            if ((p.mouseIsPressed || p.touchStarted) && !click) {
+              click = true;
+              click2 = true;
+              setTimeout(function () {
+                click2 = false;
+              }, 50);
+            }
+            p.mouseReleased = function () {
+              click = false;
+            };
+            p.touchEnded = function () {
+              click = false;
+            };
           }
         }
       }
     }
   };
 
-  async function drawSprites() {
-    if (spritesheet.width > 1 && spritedata && check) {
+  function drawSprites() {
+    if (
+      spritesheet.width > 1 &&
+      spritesheet2.width > 1 &&
+      spritedata &&
+      check
+    ) {
+      //Create animation for sprite1
       p.background(0);
-      await p.image(spritesheet, 0, 0);
+      p.image(spritesheet, 0, 0);
       let frames = spritedata.frames;
       for (let i = 0; i < frames.length; i++) {
         let pos = frames[i].position;
         let img = spritesheet.get(pos.x, pos.y, pos.w, pos.h);
         animation.push(img);
-        console.log(spritesheet.width);
       }
-      // console.log(spritedata);
+
+      //Create animation for sprite2
       p.background(0);
-      // p.image(animation[p.frameCount % animation.length], 0, 0);
+      p.image(spritesheet2, 0, 0);
+      frames = spritedata.frames;
+      for (let i = 0; i < frames.length; i++) {
+        let pos = frames[i].position;
+        let img = spritesheet2.get(pos.x, pos.y, pos.w, pos.h);
+        animation2.push(img);
+      }
+      p.background(0);
+
+      //set check to false
       check = false;
-      console.log(animation);
-      p.background(0);
     }
   }
 
   class Sprite {
-    constructor(animation, x, y, speed) {
+    constructor(animation, animation2, x, y, speed, alive) {
       this.x = x;
       this.y = y;
       this.animation = animation;
+      this.animation2 = animation2;
       this.len = this.animation.length;
+      this.len2 = this.animation2.length;
       this.speed = speed;
       this.index = 0;
+      this.alive = alive;
     }
     show() {
-      let index = p.floor(this.index) % this.len;
-      p.image(this.animation[index], this.x, this.y);
+      if (this.alive) {
+        let index = p.floor(this.index) % this.len;
+        p.image(this.animation[index], this.x, this.y);
+      }
+      if (!this.alive) {
+        let index = p.floor(this.index) % this.len2;
+        p.image(this.animation2[index], this.x, this.y);
+        if (index === 11) {
+          sticks = sticks.filter((sprite) => sprite.x !== this.x);
+        }
+      }
     }
 
     animate() {
-      this.index += this.speed;
-      this.x += this.speed * 5;
-      if (this.x > p.width) {
-        this.x = -this.animation[0].width;
+      if (this.alive) {
+        this.index += this.speed;
+        this.x += this.speed * 5;
+        if (this.x > p.width) {
+          this.x = -this.animation[0].width;
+        }
+      }
+      if (!this.alive) {
+        this.index += 0.35;
+      }
+    }
+
+    clicked() {
+      var dx = p.dist(p.mouseX, 0, this.x + this.animation[0].width / 2, 0);
+      var dy = p.dist(
+        p.mouseY,
+        0,
+        this.y + this.animation[0].height / 2 - 22,
+        0
+      );
+
+      if (
+        // use divisor greater than two to restrict click area sensitivity
+        // dx < this.animation[0].width / 2 &&
+        // dy < this.animation[0].height / 2
+        dx < this.animation[0].width / 5 &&
+        dy < this.animation[0].height / 6
+      ) {
+        if (click2) {
+          this.alive = false;
+          this.index = 0;
+        }
       }
     }
   }
