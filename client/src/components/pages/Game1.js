da8931ea3ed7d7f7d356cd2aa3865c18c155e99a
deafult2010 <thomas.jaeger@blueyonder.co.uk>
2020-05-27 11:52:00 +0100

    added rotate gif

--- client/src/components/pages/Game1.js
+++ client/src/components/pages/Game1.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import P5Wrapper from 'react-p5-wrapper';
+import Navbar from '../layout/Navbar';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -10,9 +11,11 @@ const myImage = require('./sprites/Stick.png');
 const myImage2 = require('./sprites/StickHeadShot.png');
 const myJSON = require('./sprites/stick.json');
 const mySound = require('./sounds/shot.mp3');
+const rotateGif = require('./sprites/RotateGif.gif');
 
 const Game1 = () => {
   let [openToggle, setOpenToggle] = useState(true);
+  let game;
   const menu = () => {
     setOpenToggle(!openToggle);
   };
@@ -36,6 +39,29 @@ const Game1 = () => {
     // eslint-disable-next-line
   }, []);
 
+  const size = useWindowSize();
+
+  // prompt landscape
+  if (size.width / size.height > 1.0) {
+    game = (
+      <P5Wrapper
+        sketch={sketch5}
+        menu={menu}
+        openToggle={openToggle}
+        image={myImage}
+        image2={myImage2}
+        json={myJSON}
+        sound={mySound}
+      />
+    );
+  } else {
+    game = (
+      <div className='container'>
+        <Navbar /> <img src={rotateGif} alt='loading...' />
+      </div>
+    );
+  }
+
   return (
     <div
       style={{
@@ -47,18 +73,39 @@ const Game1 = () => {
         // transform: 'translate(-50%, -50%)',
       }}
     >
-      <P5Wrapper
-        sketch={sketch5}
-        menu={menu}
-        openToggle={openToggle}
-        image={myImage}
-        image2={myImage2}
-        json={myJSON}
-        sound={mySound}
-      />
+      {game}
       <OpenMenu openToggle={openToggle} menu={menu} />
     </div>
   );
 };
+
+// Hook
+function useWindowSize() {
+  const isClient = typeof window === 'object';
+
+  function getSize() {
+    return {
+      width: isClient ? window.innerWidth : undefined,
+      height: isClient ? window.innerHeight : undefined,
+    };
+  }
+
+  const [windowSize, setWindowSize] = useState(getSize);
+
+  useEffect(() => {
+    if (!isClient) {
+      return false;
+    }
+
+    function handleResize() {
+      setWindowSize(getSize());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []); // Empty array ensures that effect is only run on mount and unmount
+
+  return windowSize;
+}
 
 export default Game1;
